6cc8270d9f40703334775efe0dd1b0e121e26545
Gothmate <gothmate@gmail.com>
2021-11-10 16:48:26 -0300

    Ip Tracker challenge, primeiro commit

---
+++ main.js
@@ -0,0 +1,33 @@
+const ipBusca = document.getElementById('ip-busca')
+
+function getIP() {
+  const ip = document.getElementsByTagName('input')[0].value
+  ipBusca.innerText = ip
+
+  const apiRequest = `https://geo.ipify.org/api/v2/country?apiKey=at_Sc05p9p1UIwJ0Si5pE7lVSzaScnkf&ipAddress=${ip}`
+
+  main(apiRequest)
+}
+
+function requestGET(url) {
+  let request = new XMLHttpRequest()
+  request.open('GET', url, false)
+  request.send()
+
+  return request.response
+}
+
+async function main(apiRequest) {
+  const isp = document.getElementById('isp')
+  const location = document.getElementById('location')
+
+  let data = requestGET(apiRequest)
+  let resposta = await JSON.parse(data)
+
+  console.log(resposta)
+
+  location.innerText = `${resposta.location.country}-${resposta.location.region} ${resposta.location.timezone}`
+  isp.innerText = resposta.isp
+
+  return resposta
+}
